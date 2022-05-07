32e10d660a18b6624a2d4abc8a1fe5db9db76e90
Elliott Shugerman <eeshugerman@gmail.com>
2022-05-07 02:50:11 -0400

    this is the way

--- Vagrantfile
+++ Vagrantfile
@@ -43,8 +43,7 @@ Vagrant.configure("2") do |config|
   # the path on the host to the actual folder. The second argument is
   # the path on the guest to mount the folder. And the optional third
   # argument is a set of non-required options.
-  config.vm.synced_folder "../gnome-shell", "/home/vagrant/src/gnome-shell",
-                          owner: "vagrant", group: "vagrant" # doesn't work with virtualbox?
+  config.vm.synced_folder "../gnome-shell", "/home/vagrant/src/gnome-shell" #, owner: "vagrant", group: "vagrant" # doesn't work with virtualbox?
 
   # Provider-specific configuration so you can fine-tune various
   # backing providers for Vagrant. These expose provider-specific options.
--- provision.sh
+++ provision.sh
@@ -19,32 +19,8 @@ sudo dnf install -y \
      polkit-devel \
      startup-notification-devel \
      ibus-devel \
-     pipewire-devel
-     # mozjs78-devel \
-     # gsettings-desktop-schemas-devel \
-     # glib2-devel
-
-
-
-
-## alternatively, don't boot to graphical. may not be needed on fresh provision.
-# haven't figured out how to start gnome session from ssh (either x or wayland) so this is useless for now.
-# systemctl set-default multi-user.target
-
-# based on https://gitlab.gnome.org/GNOME/gnome-shell/-/snippets/1515
-# gnome_libs='gsettings-desktop-schemas libgweather gjs glib mutter'
-# gnome_libs='gsettings-desktop-schemas gjs glib mutter'
-# freedesktop_libs='pipewire'
-
-# sudo chown $USER $HOME/src
-
-# for lib in $gnome_libs; do
-#     lib_dir="$HOME/src/$lib"
-#     # if [! -d $lib_dir ]; then
-#     #     git clone -b 41.0 "https://gitlab.gnome.org/GNOME/$lib" $lib_dir
-#     #     cd $lib_dir
-#     # fi
-#     cd $lib_dir
-#     meson --prefix=/usr build
-#     sudo ninja -C build install -j1
-# done
+     pipewire-devel \
+     NetworkManager-libnm-devel \
+     rust-libpulse-sys+pa_v12-devel \
+     gtk4-devel \
+     sassc
